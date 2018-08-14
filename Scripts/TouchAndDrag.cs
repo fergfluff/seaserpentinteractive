bfd2bc0daefa7bc839ba8454101b3b09ba5a3ff0
Elizabeth Ferguson <elizabeth.j.ferguson@gmail.com>
2018-08-13 22:14:39 -0400

    Add files via upload

---
+++ Scripts/TouchAndDrag.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchAndDrag : MonoBehaviour {
+
+	//GameObjects
+	public GameObject ocean;
+	public GameObject attractText;
+	public GameObject explanationText;
+
+	//Components
+	private float originalY;
+	public Transform upperLimit;
+	public Transform lowerLimit;
+	public SpringJoint2D spring;
+	public CanvasGroup attractCanvasGroup;
+	public CanvasGroup explanationTextCanvasGroup;
+
+	//store a speed to slow movement of TouchZone in response to the user's touch
+	public float speed = .1F;
+
+	void Start () {
+		//use Start() to store a static copy of the touchZone's Y
+		//to be able to return the touchZone to its original place
+		//after the touch event
+		originalY = transform.position.y;
+	}
+
+	void Update () {
+		attractCanvasGroup = attractText.GetComponent<CanvasGroup>();
+		explanationTextCanvasGroup = explanationText.GetComponent<CanvasGroup>();
+
+		//If there is more than one touch
+		//And if the movement of the first touch has changed
+		if (Input.touchCount > 0) {
+
+			//And if the touch has started to move
+			if (Input.GetTouch(0).phase == TouchPhase.Began) {
+			
+
+			//Otherwise, if the touch keeps moving
+			} else if (Input.GetTouch(0).phase == TouchPhase.Moved) {	
+
+				//Store value of the touch's change in position
+				Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+				float maximumTouchDistance = Mathf.Min(touchDeltaPosition.y, 10);
+
+				//And if the touch's y position is in the middle of the screen
+				//Move the TouchZone game object down the screen
+				//And alpha out / in the text boxes
+				if (touchDeltaPosition.y < upperLimit.position.y && touchDeltaPosition.y > lowerLimit.position.y) {
+					Debug.Log("Touch is in range");
+					transform.Translate(0, maximumTouchDistance, 0);
+					attractCanvasGroup.alpha -= .05f;
+					explanationTextCanvasGroup.alpha += .1f;
+
+				//Or if the touch's y is close to the bottom of the screen
+				//Don't let it go down & off the screen
+				} else if (touchDeltaPosition.y < lowerLimit.position.y) {
+					Debug.Log("Touch is below range");
+					transform.position = new Vector3(transform.position.x, lowerLimit.position.y, transform.position.z);
+					attractCanvasGroup.alpha -= .1f;
+					explanationTextCanvasGroup.alpha += .1f;
+
+				//Or if the touch's y is towards the top
+				//Don't let it go up & off the screen
+				} else if (touchDeltaPosition.y > upperLimit.position.y) {
+					Debug.Log("Touch is above range");
+					transform.position = new Vector3(transform.position.x, upperLimit.position.y, transform.position.z);
+				}
+
+			//Or if the touch has stopped moving
+			} else if (Input.GetTouch(0).phase == TouchPhase.Ended) {
+			}
+
+		//Or if the touch has let go
+		} else {
+				moveTouchZoneBack();
+		}
+	}
+
+	void moveTouchZoneBack() {
+		// Debug.Log("Trying to move touchZone");
+
+		float currentDistance = originalY - transform.position.y;
+		float newY = currentDistance * 0.1f;
+
+		transform.Translate(0, newY, 0);
+		attractCanvasGroup.alpha += .1f;
+		explanationTextCanvasGroup.alpha -= .1f;
+	}
+}
+
---
+++ Scripts/handMovement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//UICircle moves up repeatedly, starting at the same location
+//As it moves up, it changes from its original gray to a transparent shade
+//Perhaps it should be white?
+//When the user touches the screen, the circle disappears
+//When the user lets go, the circle appears after a couple seconds
+
+
+public class handMovement : MonoBehaviour {
+
+	//Game Objects
+	public GameObject Hand;
+
+	//Components
+	public CanvasGroup HandCanvasGroup;
+	
+
+	//Values
+	public float speed;
+	public float distance = 50;
+	public float alphaRate = 0.007f;
+	private float originalY;
+
+	// Use this for initialization
+	void Start () {
+		originalY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandCanvasGroup = Hand.GetComponent<CanvasGroup>();
+
+        //&& Input.GetTouch(0).phase == TouchPhase.Moved
+
+        //If there is more than one touch
+        //And if the movement of the first touch has changed
+        if (Input.touchCount > 0) {
+			Debug.Log("User is touching screen");
+			transform.position = new Vector3(transform.position.x, originalY, transform.position.z);
+            //Make it disappear
+            HandCanvasGroup.alpha = 0f;
+		} else {
+			Debug.Log("User is NOT touching screen");
+			//If user is not touching screen
+			//Make circle move repeatedly down, starting from the same location
+			moveCircle(); 
+		}
+	}
+
+	void moveCircle() {
+        Debug.Log("Hand appears and starts to move again");
+		//distance between the circle's original starting point and its current location
+		float currentDistance = originalY - transform.position.y;
+
+	
+		//If the UICircle passes a certain distance from its starting point
+		//Keep moving it down
+		//Slow it down a lot when it starts and stops, and goes a little faster in the middle
+		//And make it fade away
+
+		//if currentDistance is greater than 10
+		if (currentDistance < distance - 90) {
+			transform.Translate(Vector3.down * Time.deltaTime*35);
+            HandCanvasGroup.alpha -= alphaRate;
+		//if currentDistance is less than 100
+		} else if (currentDistance < distance) {
+			transform.Translate(Vector3.down * Time.deltaTime*60);
+            HandCanvasGroup.alpha -= alphaRate;
+		//if currentDistance is less than 105
+		} else if (currentDistance < distance + 5) {
+			transform.Translate(Vector3.down * Time.deltaTime*40);
+            HandCanvasGroup.alpha -= alphaRate;
+		//if currentDistance is less than 115
+		} else if (currentDistance < distance + 15) {
+			transform.Translate(Vector3.down * Time.deltaTime*30);
+            HandCanvasGroup.alpha -= alphaRate;
+		//Put it back where it started
+		} else {
+            Debug.Log("Hand appears again");
+			transform.position = new Vector3(transform.position.x, originalY, transform.position.z);
+            HandCanvasGroup.alpha = 1f;
+		}
+	}
+}
+
+
+
